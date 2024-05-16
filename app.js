089b07c48bc2db9f6a7c82877f9f8486642594e2
Lucass-Dev <liger.lucas33@gmail.com>
2024-05-16 21:08:51 +0200

    init bot

---
+++ app.js
@@ -0,0 +1,37 @@
+// Require the necessary discord.js classes
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+// const database = require('./testConnexion.js');
+
+// // Create a new client instance
+ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// database.connexion(client);
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+// When the client is ready, run this code (only once)
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	} 
+}
+
+
+// Login to Discord with your client's token
+client.login(token);
