299d5414bb258cb9ca16b9bf2edb2191158c0053
BaptisteB14 <blanchet.baptiste77@gmail.com>
2024-09-07 21:53:11 +0200

    nettoyage

--- app.js
+++ app.js
@@ -6,34 +6,12 @@ import {
   verifyKeyMiddleware,
 } from 'discord-interactions';
 
-import { joinVoiceChannel, createAudioPlayer, createAudioResource } from '@discordjs/voice';
-
-import {
-  Client,
-  GatewayIntentBits
-}from 'discord.js';
-
 // Create an express app
 const app = express();
 // Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
 
-// Initialisation du client Discord
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates, // Nécessaire pour écouter les états vocaux
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent, // Nécessaire pour lire le contenu des messages
-  ],
-});
 
-client.once('ready', () => {
-  console.log(`Bot connecté en tant que ${client.user.tag}`);
-});
-
-// Lancer le bot
-client.login(process.env.DISCORD_TOKEN);
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  * Parse request body and verifies incoming requests using discord-interactions package
